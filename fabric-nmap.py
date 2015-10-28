866769b0a3b83e7f54d17e47f028be04555e60e4
Benoit FOUJOLS <bfoujols@workit.fr>
2015-10-28 11:01:50 +0100

    [Added] Active command Ping

--- fabric-nmap.py
+++ fabric-nmap.py
@@ -6,9 +6,8 @@ Source code : https://github.com/bfoujols/fabric-nmap.git
 """
 
 __author__ = 'Benoit Foujols (benoit@foujols.com)'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __last_modification__ = '2015.10.06'
-
 
 import optparse
 import nmap
@@ -18,21 +17,21 @@ import csv
 from fabric.api import *
 from fabric.network import disconnect_all
 
-#import paramiko, os
-#paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
+# import paramiko, os
+# paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
 
 class reportNetwork:
     """
     Class reportNetwork
     Recover IP for NMAP
     """
+
     def __init__(self, hosts='localhost'):
         """
         Init reportNetwork
         :param hosts: string IP ex 192.168.0.1 or 192.168.2.0/24
         """
         self.hosts = hosts
-
 
     def getNetwork(self):
         """
@@ -54,6 +53,21 @@ class reportNetwork:
 
         return hosts
 
+    def getPingUp(self):
+        try:
+            self.nmap = nmap.PortScanner()
+        except nmap.PortScannerError:
+            print("ERROR-005 NMAP :", sys.exc_info()[0])
+            sys.exit(0)
+        except:
+            print("ERROR-004 :", sys.exc_info()[0])
+            sys.exit(0)
+
+        self.nmap.scan(self.hosts, arguments='-sP')
+        hosts = self.nmap.all_hosts()
+        #hosts = [(x, self.nmap[x]['status']['state']) for x in self.nmap.all_hosts()]
+
+        return hosts
 
 
 class reportHost:
@@ -61,6 +75,7 @@ class reportHost:
     Class reportHost
     Executed Fabric Script
     """
+
     def __init__(self, hosts='', login='', password='', command='', sudo_commande=''):
         """
         Init reportHost
@@ -76,7 +91,7 @@ class reportHost:
                 env.hosts = hosts
 
             def fabricEnv():
-                env.skip_bad_hosts=True
+                env.skip_bad_hosts = True
                 env.warn_only = True
                 env.user = login
                 env.password = password
@@ -98,12 +113,12 @@ class reportHost:
             print "ERROR-003 : Missing an argument"
 
 
-
 class inputCsv:
     """
     Class inputCsv
     Input file Host CSV
     """
+
     def __init__(self, filecsv=''):
         """
         Init inputCsv
@@ -138,7 +153,6 @@ class inputCsv:
             sys.exit(0)
 
 
-
 def getArg():
     """
     Get the argument
@@ -146,6 +160,7 @@ def getArg():
 
     :return: (options, args)
 
+    options.getarg_ping Option -P
     options.getarg_version Option -v
     options.getarg_verbose Option -q
     options.getarg_csv Option -c
@@ -167,6 +182,7 @@ def getArg():
     parser.add_option("-c", "--inputcsv", dest="getarg_csv", help="Enter input CSV file", metavar="FILE")
     parser.add_option("-q", "--verbose", dest="getarg_verbose", help="Active mode debug", action="store_true")
     parser.add_option("-v", "--version", dest="getarg_version", help="See app version", action="store_true")
+    parser.add_option("-P", "--ping", dest="getarg_ping", help="Enter your filename", metavar="FILENAME")
 
     (options, args) = parser.parse_args()
 
@@ -175,47 +191,62 @@ def getArg():
         print "VERSION ", __version__
         print "LAST_MODIFICATION ", __last_modification__
         sys.exit(0)
-    elif not options.getarg_command:
-        print "ERROR-001 : You must first the command - Option -C <COMMAND>"
+    elif not (options.getarg_command or options.getarg_ping):
+        print "ERROR-001 : You must first the command - Option -C <COMMAND> OR Option -P <FILENAME>"
         sys.exit(0)
-    elif options.getarg_login:
-        login = options.getarg_login
-        password = getpass.getpass('Enter your password : ')
-    else:
+    elif options.getarg_command and not options.getarg_login:
         print "ERROR-002 : You must first your login SSH - Option -u <LOGIN>"
         sys.exit(0)
 
-    return options.getarg_sudo, options.getarg_verbose, options.getarg_csv, options.getarg_host, options.getarg_command, login, password
+    if options.getarg_login:
+        login = options.getarg_login
+        password = getpass.getpass('Enter your password : ')
+    else:
+        login = False
+        password = False
 
+    return options.getarg_ping, options.getarg_sudo, options.getarg_verbose, options.getarg_csv, options.getarg_host, options.getarg_command, login, password
 
 
 def main():
-    sudo_commande, verbose, filecsv, host, command, login, password = getArg()
+    ping_commande, sudo_commande, verbose, filecsv, listhost, command, login, password = getArg()
 
     if verbose:
         print "**** ARG **************************"
-        print "Host:",host
-        print "CSV:",filecsv
-        print "COMMAND:",command
-        print "LOGIN:",login
+        print "Host:", listhost
+        print "CSV:", filecsv
+        print "COMMAND:", command
+        print "LOGIN:", login
         print "**** ARG end **********************"
 
-    if host:
-        report = reportNetwork(host)
-        hosts = report.getNetwork()
-    elif filecsv:
-        inputfile = inputCsv(filecsv)
-        hosts = inputfile.getHostToCsv()
-    else:
-        print "Please, select the option -c or -H"
+    if sudo_commande:
+        if listhost:
+            report = reportNetwork(listhost)
+            if ping_commande:
+                allhost = report.getPingUp()
+            else:
+                allhost = report.getNetwork()
+        elif filecsv:
+            inputfile = inputCsv(filecsv)
+            allhost = inputfile.getHostToCsv()
+
+        reportHost(allhost, login, password, command, sudo_commande)
+
+    elif ping_commande:
+        report = reportNetwork(listhost)
+        allhost = report.getPingUp()
+
+        resultFyle = open(ping_commande + ".csv",'wb')
+        wr = csv.writer(resultFyle, dialect='excel')
+
+        for hostPing in allhost:
+            wr.writerow([hostPing,])
+
 
     if verbose:
         print "**** LIST Host **************************"
-        print hosts
+        print allhost
         print "**** LIST Host end **********************"
-
-    reportHost(hosts, login, password, command, sudo_commande)
-
 
 
 if __name__ == "__main__":
